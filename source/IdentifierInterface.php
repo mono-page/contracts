76e167caf0a63996cc2114df011025a1c6a980de
Vitaliy Khomenko <iiifx.me@gmail.com>
2019-12-14 15:04:59 +0200

    fix(repo): Переименован в контракты

---
+++ source/IdentifierInterface.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace MonoPage\Contracts;
+
+interface IdentifierInterface
+{
+}
--- source/RepositoryInterface.php
+++ source/RepositoryInterface.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace MonoPage\Core\Interfaces;
+namespace MonoPage\Contracts;
 
 interface RepositoryInterface
 {
